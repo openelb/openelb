ebd4ed99f1435c64b6ecab91c58db57727347f60
Magic Song <magicong@yunfiy.com>
2019-02-19 23:37:14 +0800

    add tool install

---
+++ hack/install_tools.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+echo "install kubebuilder"
+
+version=1.0.8 # latest stable version
+arch=amd64
+
+# download the release
+curl -L -O "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v${version}/kubebuilder_${version}_linux_${arch}.tar.gz"
+
+# extract the archive
+tar -zxvf kubebuilder_${version}_linux_${arch}.tar.gz
+sudo mv kubebuilder_${version}_linux_${arch} /usr/local/kubebuilder
+
+# update your PATH to include /usr/local/kubebuilder/bin
+export PATH=$PATH:/usr/local/kubebuilder/bin
+
+echo "install kustomize"
+
+wget https://github.com/kubernetes-sigs/kustomize/releases/download/v1.0.11/kustomize_1.0.11_linux_amd64 
+chmod u+x kustomize_1.0.11_linux_amd64
+sudo mv kustomize_1.0.11_linux_amd64 /usr/local/bin/kustomize
+
+echo "Tools install done"
--- hack/tool.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-go build -o bin/routeprint tools/route_print.go
-scp bin/routeprint root@192.168.98.2:/root/
-
-ssh root@192.168.98.2 "./routeprint lo"
