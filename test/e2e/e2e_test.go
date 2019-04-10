246681d3da7f73101c4e3e52b7653b17dd412319
magicsong <magicsong@yunify.com>
2019-04-10 12:24:39 +0800

    fix grammar

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -52,7 +52,7 @@ var _ = Describe("e2e", func() {
 		Expect(thisTestCase.DeployYaml()).ShouldNot(HaveOccurred(), "Failed to deploy yaml")
 		defer func() {
 			Expect(e2eutil.KubectlDelete(thisTestCase.DeployYamlPath)).ShouldNot(HaveOccurred(), "Failed to delete yaml")
-			Expect(e2eutil.EnsureNamespaceClean(thisTestCase.Namespace).ShouldNot(HaveOccurred())
+			Expect(e2eutil.EnsureNamespaceClean(thisTestCase.Namespace)).ShouldNot(HaveOccurred())
 		}()
 
 		pod := &corev1.Pod{}
