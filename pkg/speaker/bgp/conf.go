a7f1d13a2230085f7c1125cf1cea629a212badda
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-08-20 00:10:49 +0530

    style(pkg/bgp): replace BgpConf with api.Global for type conversion
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- pkg/speaker/bgp/conf.go
+++ pkg/speaker/bgp/conf.go
@@ -6,26 +6,32 @@ import (
 	"os"
 
 	bgpapi "github.com/openelb/openelb/api/v1alpha2"
+	api "github.com/osrg/gobgp/api"
 	"github.com/osrg/gobgp/pkg/config"
 	"github.com/spf13/viper"
 )
 
-func (b *Bgp) HandleBgpGlobalConfig(global *bgpapi.BgpConf, rack string, delete bool) error {
+func (b *Bgp) HandleBgpGlobalConfig(bgpConf *bgpapi.BgpConf, rack string, delete bool) error {
 	b.rack = rack
 	if delete {
 		return b.bgpServer.StopBgp(context.Background(), nil)
 	}
-	return b.CreateOrUpdateBgpConfig(global)
+	apiGlobal, err := bgpConf.Spec.ToGoBgpGlobalConf()
+	if err != nil {
+		return err
+	}
+	globalMap := GetBgpConfigGlobalMap(apiGlobal)
+	return b.CreateOrUpdateBgpConfig(globalMap, apiGlobal.GetGracefulRestart().GetEnabled())
 }
 
-func (b *Bgp) CreateOrUpdateBgpConfig(bgpConf *bgpapi.BgpConf) error {
+func (b *Bgp) CreateOrUpdateBgpConfig(globalMap map[string]interface{}, gracefulRestart bool) error {
 	if _, err := os.Stat(b.v.ConfigFileUsed()); errors.Is(err, os.ErrNotExist) {
 		if _, err := os.Create(b.v.ConfigFileUsed()); err != nil {
 			return err
 		}
 	}
 	viper := viper.New()
-	if err := viper.MergeConfigMap(GetBgpConfigGlobalMap(bgpConf)); err != nil {
+	if err := viper.MergeConfigMap(globalMap); err != nil {
 		return err
 	}
 	if err := b.v.MergeConfigMap(viper.AllSettings()); err != nil {
@@ -41,25 +47,30 @@ func (b *Bgp) CreateOrUpdateBgpConfig(bgpConf *bgpapi.BgpConf) error {
 	if err != nil {
 		return err
 	}
-	_, err = config.InitialConfig(context.Background(), b.bgpServer, bcs, false)
+	_, err = config.InitialConfig(context.Background(), b.bgpServer, bcs, gracefulRestart)
 	return err
 }
 
-func GetBgpConfigGlobalMap(bgpConf *bgpapi.BgpConf) map[string]interface{} {
-	// _, err = config.InitialConfig(context.Background(), b.bgpServer, bcs, false)
+func GetBgpConfigGlobalMap(apiGlobal *api.Global) map[string]interface{} {
 	return map[string]interface{}{
-		"global.config.as":                 bgpConf.Spec.As,
-		"global.config.router-id":          bgpConf.Spec.RouterId,
-		"global.config.port":               bgpConf.Spec.ListenPort,
-		"global.config.local-address-list": bgpConf.Spec.ListenAddresses,
-	}
-}
-
-func GetBgpConfigNeighborMap(bgpConf *bgpapi.BgpConf) map[string]interface{} {
-	return map[string]interface{}{
-		"global.config.as":                 bgpConf.Spec.As,
-		"global.config.router-id":          bgpConf.Spec.RouterId,
-		"global.config.port":               bgpConf.Spec.ListenPort,
-		"global.config.local-address-list": bgpConf.Spec.ListenAddresses,
+		// config
+		"global.config.as":                 apiGlobal.GetAs(),
+		"global.config.router-id":          apiGlobal.GetRouterId(),
+		"global.config.port":               apiGlobal.GetListenPort(),
+		"global.config.local-address-list": apiGlobal.GetListenAddresses(),
+		// // use-multiple-paths
+		// "globa.use-multiple-paths.config.enabled": apiGlobal.GetUseMultiplePaths(),
+		// // graceful-restart
+		// "global.graceful-restart.config.enabled":              apiGlobal.GetGracefulRestart().GetEnabled(),
+		// "global.graceful-restart.config.restart-time":         apiGlobal.GetGracefulRestart().GetRestartTime(),
+		// "global.graceful-restart.config.stale-routes-time":    apiGlobal.GetGracefulRestart().GetStaleRoutesTime(),
+		// "global.graceful-restart.config.helper-only":          apiGlobal.GetGracefulRestart().GetHelperOnly(),
+		// "global.graceful-restart.config.peer-restart-time":    apiGlobal.GetGracefulRestart().GetPeerRestartTime(),
+		// "global.graceful-restart.config.peer-restarting":      apiGlobal.GetGracefulRestart().GetPeerRestarting(),
+		// "global.graceful-restart.config.local-restarting":     apiGlobal.GetGracefulRestart().GetLocalRestarting(),
+		// "global.graceful-restart.config.mode":                 apiGlobal.GetGracefulRestart().GetMode(),
+		// "global.graceful-restart.config.deferral-time":        apiGlobal.GetGracefulRestart().GetDeferralTime(),
+		// "global.graceful-restart.config.notification-enabled": apiGlobal.GetGracefulRestart().GetNotificationEnabled(),
+		// "global.graceful-restart.config.long-lived-enabled":   apiGlobal.GetGracefulRestart().GetLonglivedEnabled(),
 	}
 }
